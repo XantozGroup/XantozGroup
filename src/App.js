9b3062d2a2b2c883ba1ad787c9e44d1cd5c9f31e
Gabbie99 <khoinom2018@gmail.com>
2022-11-17 10:48:55 +0100

    chore: product page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './homePage/NavBar';
 import HomePage from './homePage/HomePage'
 import About from './aboutPage/About';
+import Product from './productPage/Product';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar/>
       <HomePage/>
       <About/>
+      <Product/>
     </div>
   );
 }
