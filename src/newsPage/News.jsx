63a00f374dbed6538906fb088725b19f45a63fe1
Gabbie99 <khoinom2018@gmail.com>
2022-11-17 15:37:06 +0100

    chore:on news feed inprogress

--- src/newsPage/News.jsx
+++ src/newsPage/News.jsx
@@ -3,6 +3,11 @@ import './News.css'
 import agbada from '../assets/agbada.jpg'
 import ashSuit from '../assets/ash-suit.jpg'
 import blueSuit from '../assets/blue-suit.jpg'
+import blackcoorp from '../assets/black-coorp.jpg'
+import jean from '../assets/jean-gown.jpg'
+import orangeT from '../assets/orange-trouser.jpg'
+import gownSuit from '../assets/gown-suit.png'
+import manStyle from '../assets/man-style.png'
 
 function News() {
   return (
@@ -41,7 +46,87 @@ function News() {
             </div>
             <div className='flex-news'>
               <div>
-                <img src={blueSuit} alt="" />
+                <img src={blueSuit} alt="blue" />
+              </div>
+              
+              <div className='text-news'>
+                <h3>News Trend</h3>
+                <div className='news-p'>
+                  <p>17th Nov 2022</p>
+                  <p>Like & Comment</p>
+                </div>
+                <p className='news-tell'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi omnis quibusdam quam perferendis ut corporis? Consectetur laboriosam placeat, maxime praesentium nulla atque sint, totam dolorum est ducimus repellendus. Dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In magnam eius voluptatum nostrum, cupiditate dolorum aperiam, qui doloremque distinctio quae exercitationem ea ab totam, officiis impedit amet voluptatem? Consequatur, maiores?
+                </p>
+              </div>
+            </div>
+            <div className='flex-news'>
+              <div>
+                <img src={blackcoorp} alt="black" />
+              </div>
+              
+              <div className='text-news'>
+                <h3>News Trend</h3>
+                <div className='news-p'>
+                  <p>17th Nov 2022</p>
+                  <p>Like & Comment</p>
+                </div>
+                <p className='news-tell'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi omnis quibusdam quam perferendis ut corporis? Consectetur laboriosam placeat, maxime praesentium nulla atque sint, totam dolorum est ducimus repellendus. Dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In magnam eius voluptatum nostrum, cupiditate dolorum aperiam, qui doloremque distinctio quae exercitationem ea ab totam, officiis impedit amet voluptatem? Consequatur, maiores?
+                </p>
+              </div>
+            </div>
+            <div className='flex-news'>
+              <div>
+                <img src={jean} alt="jean" />
+              </div>
+              
+              <div className='text-news'>
+                <h3>News Trend</h3>
+                <div className='news-p'>
+                  <p>17th Nov 2022</p>
+                  <p>Like & Comment</p>
+                </div>
+                <p className='news-tell'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi omnis quibusdam quam perferendis ut corporis? Consectetur laboriosam placeat, maxime praesentium nulla atque sint, totam dolorum est ducimus repellendus. Dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In magnam eius voluptatum nostrum, cupiditate dolorum aperiam, qui doloremque distinctio quae exercitationem ea ab totam, officiis impedit amet voluptatem? Consequatur, maiores?
+                </p>
+              </div>
+            </div>
+            <div className='flex-news'>
+              <div>
+                <img src={orangeT} alt="orangeT" />
+              </div>
+              
+              <div className='text-news'>
+                <h3>News Trend</h3>
+                <div className='news-p'>
+                  <p>17th Nov 2022</p>
+                  <p>Like & Comment</p>
+                </div>
+                <p className='news-tell'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi omnis quibusdam quam perferendis ut corporis? Consectetur laboriosam placeat, maxime praesentium nulla atque sint, totam dolorum est ducimus repellendus. Dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In magnam eius voluptatum nostrum, cupiditate dolorum aperiam, qui doloremque distinctio quae exercitationem ea ab totam, officiis impedit amet voluptatem? Consequatur, maiores?
+                </p>
+              </div>
+            </div>
+            <div className='flex-news'>
+              <div>
+                <img src={gownSuit} alt="gown" />
+              </div>
+              
+              <div className='text-news'>
+                <h3>News Trend</h3>
+                <div className='news-p'>
+                  <p>17th Nov 2022</p>
+                  <p>Like & Comment</p>
+                </div>
+                <p className='news-tell'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi omnis quibusdam quam perferendis ut corporis? Consectetur laboriosam placeat, maxime praesentium nulla atque sint, totam dolorum est ducimus repellendus. Dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In magnam eius voluptatum nostrum, cupiditate dolorum aperiam, qui doloremque distinctio quae exercitationem ea ab totam, officiis impedit amet voluptatem? Consequatur, maiores?
+                </p>
+              </div>
+            </div>
+            <div className='flex-news'>
+              <div>
+                <img src={manStyle} alt="man" />
               </div>
               
               <div className='text-news'>
