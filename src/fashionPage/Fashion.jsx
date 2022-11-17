5468cb4de44b5df9e242eaea95a90d051e355d02
Gabbie99 <khoinom2018@gmail.com>
2022-11-17 13:23:40 +0100

    chore: fashion page

--- src/fashionPage/Fashion.jsx
+++ src/fashionPage/Fashion.jsx
@@ -4,17 +4,19 @@ import Hatgirl from '../assets/girl-in-hat.png'
 
 function Fashion() {
   return (
-    <div>
-        <div>
+    <div className='fashion-body'>
+        <h1 className='world'>World of Fashion</h1>
+        <div className='fashion'>
             <div className='fashion-text'>
                 <h1>Fashion</h1>
-                <button>Shop now</button>
+                <button className='text-but'>Shop now</button>
             </div>
             <div>
-                <img src={Hatgirl} alt="" />
+                <img src={Hatgirl} alt="hatgirl" />
             </div>
             <div className='fashion-text'>
                 <h1>Collection</h1>
+                <button className='shop'></button>
             </div>
         </div>
     </div>
